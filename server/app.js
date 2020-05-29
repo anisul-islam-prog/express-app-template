94c2047fbb01c9c7eef32115bae4c7264ee40212
anisul-islam-prog <anis@DESKTOP-NO1L840.localdomain>
2020-05-29 11:45:12 +0600

    :recycle:

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const port = 4000;
+
+app.listen(port, ()=>{
+    console.log("Server listenting on http://127.0.0.1:%d",port);
+});
+
+export default app;
